6a02bec7bad0d621b4c9d94be8cd57f591b123fb
Merna Bassem <mernabassem21@gmail.com>
2024-05-23 14:49:02 +0300

    update posts by user with use midleware authenticate

--- src/Modules/Posts/post.controller.js
+++ src/Modules/Posts/post.controller.js
@@ -1,15 +1,43 @@
 import Post from "../../../DB/Models/posts.model.js";
 
-export const createPost = async (req,res,next) => {
-     const  {title,content} = req.body 
-     try {
-        const post = await Post.create({
+export const createPost = async (req, res, next) => {
+  const { title, content } = req.body;
+  try {
+    const post = await Post.create({
+      title,
+      content,
+      UserId: req.userId,
+    });
+    return res.status(201).json({ message: "Post created Successful", post });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export const updatePost = async (req, res, next) => {
+  const { title, content } = req.body;
+  const post = await Post.findByPk(req.params.id);
+  if (post === null) {
+    return res.status(400).json({ message: "Post Not Found" });
+  } else {
+    if (post.UserId === req.userId) {
+      const updatePost = await Post.update(
+        {
           title,
           content,
-          UserId:req.userId,
-        });
-        return res.status(201).json({message:"Post created Successful",post})
-     }catch(error){
-        return res.status(400).json({error:error.message})
-     }
-}
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      );
+
+      return res.status(201).json({ message: "The Post Updated Successfully" });
+    } else {
+      return res.status(400).json({
+        message: "No one is allowed to edit posts of people other than his own",
+      });
+    }
+  }
+};
--- src/Modules/Posts/post.routers.js
+++ src/Modules/Posts/post.routers.js
@@ -6,5 +6,6 @@ import authenticate from "../../Midleware/authentication.midleware.js";
 const router = Router()
 
 router.post("/createPost", authenticate, PostController.createPost);
+router.put("/updatePost/:id", authenticate, PostController.updatePost);
 
 export default router;
